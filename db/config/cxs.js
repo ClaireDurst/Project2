251ab8c94bdc28ad7c4bc458191392e0ffdd88bd
James Litherland <jamilth@gmail.com>
2017-09-10 02:37:12 -0400

    basic backend

---
+++ db/config/cxs.js
@@ -0,0 +1,22 @@
+/*
+ *  (Project 2):
+ *      cxs.js - database connection/authentification
+ *
+ *------------------------------------->8------------------------------------*/
+
+var mysql = require('mysql');
+var path = require('path');
+
+var creds = {
+    host: "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "	givj98e77xcypqaw",
+    password: "pba0j70rtzbq6pmx",
+    database: "hr0vdec8yom4q8ik",
+    stringifyObjects: true
+}
+
+var cxs = mysql.createConnection(creds);
+
+cxs.connect();
+
+module.exports(cxs);
---
+++ db/config/orm.js
@@ -0,0 +1,7 @@
+/*
+ *  (Project 2):
+ *      orm.js - ya dont want to know, buddy.
+ *
+ *-------------------------------------->8------------------------------------*/
+
+ var path-  gh7ul,mniuokl,. jklp[;olm ]
