107cc53a8f33cf31e4cb1f85e40ee13ffaa33482
James Litherland <jamilth@gmail.com>
2017-09-10 07:48:55 -0400

    =Ya

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,31 @@
  *-------------------------------------->8------------------------------------*/
 console.log('+main.js');
 
- $(document).ready(function() {
+function loginStatus(response) {
+    console.print(response);
+}
 
-    console.log(LOGGED_IN)
+ $(document).ready(function() {
+     window.fbAsyncInit = function () {
+         FB.init({
+             appId: '103017880442508',
+             cookie: true,
+             xfbml: true,
+             version: 'v2.10'
+         });
+         FB.AppEvents.logPageView();
+         FB.getLoginStatus((resp) => {
+            loginStatus(resp);
+         });
+     };
+
+     (function (d, s, id) {
+         var js, fjs = d.getElementsByTagName(s)[ 0 ];
+         if (d.getElementById(id)) { return; }
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
+
  });
