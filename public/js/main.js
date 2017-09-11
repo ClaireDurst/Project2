a844c48d1f14040d664f167cef9dc922b3fc5951
James Litherland <jamilth@gmail.com>
2017-09-11 12:03:48 -0400

    :(

--- public/js/main.js
+++ public/js/main.js
@@ -5,14 +5,17 @@
  *-------------------------------------->8------------------------------------*/
 console.log('+main.js');
 
+function logOutFB() {
+    FB.logout();
+    $('#login_button').html('<a href="#" class="btn btn-outline-success" onClick="FB.login(function(response) { loginStatus(response); }, {scope: \'public_profile, email\'});">Login with Facebook</a>');
+}
 function loginStatus(status) {
     if (status.status == "connected") {
         FB.api('/me', { fields: 'first_name,email' }, (response) => {
             console.log('Hey ' + response.first_name + "! " + response.email);
             $.post('/login', { user: response.first_name, email: response.email }, (data) => {
                 if (data) {
-                    console.log(data);
-                    console.log('Verified user is in DB, all is well.');
+                    $('#login_button').html('<a href="#" class="btn btn-outline-success" onClick="logOutFB()">Log Out</a>');
                 }
             });
         });
